0312d941b9bcce41d29c62ad9188da50ec9694f6
Julian Pettit <julian.s.pettit@gmail.com>
2016-05-17 10:49:17 -0400

    loader works

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {
- 
-    setTimeout(function(){
-        $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 3000);
- 
+//Executes the preloading screen until page is fully loaded  
+$(document).ready(function() {    
+  $(window).load(function() {
+    $('body').addClass('loaded');
+  }); 
 });
